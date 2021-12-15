c92a98e5658e7e97ee0a2ceb3a4ddde1a6665c3f
illarjonov <danilgrot0@gmail.com>
2021-12-15 08:25:09 +0300

    main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
 import React from 'react';
 import './App.css';
 import {Routes, Route} from 'react-router-dom'
-import {Navbar} from './components/NavBar.jsx'
+import {Navbar} from './components/NavBar.js'
 import {Home} from './pages/Home';
 import {About} from './pages/About';
 import {Profile} from './pages/Profile';
+import {Alert} from './components/Alert'
+import {AlertState} from './context/alert/alertState'
 
 function App() {
   return (
-    <div>
-        <Navbar/>
-    <div className="container pt-4">
-        <Routes>
-          <Route path = "/" exact element = {<Home/>}/>
-          <Route path = "/about" element = {<About/>}/>
-          <Route path = "/profile/:id" element = {<Profile/>}/>
-        </Routes>
-    </div>
-  </div>
+    <AlertState>
+        <div>
+            <Navbar/>
+        <div className="container pt-4">
+            <Alert alert={ {text: 'test alert'} }/>
+            <Routes>
+              <Route path = "/"  element = {<Home/>}/>
+              <Route path = "/about" element = {<About/>}/>
+              <Route path = "/profile/:id" element = {<Profile/>}/>
+            </Routes>
+        </div>
+      </div>
+  </AlertState>
   );
 }
 
---
+++ src/components/Alert.js
@@ -0,0 +1,19 @@
+import React, {useContext} from 'react'
+import {AlertContext} from '../context/alert/alertContext'
+
+export const Alert = () => {
+  const {alert, hide} = useContext(AlertContext) //передать пропсы
+
+  if (!alert) return null
+  return (
+    <div
+      className={`alert alert-${alert.type || 'secondary'} alert-dismissible`}
+      role="alert"
+    >
+      {alert.text}
+      <button type="button" className="close" aria-label="close" onClick ={hide}>
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+  )
+}
